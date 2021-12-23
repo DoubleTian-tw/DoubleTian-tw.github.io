5f4e926ae7a807ac6a6bdf50255d2a1569eaf0e2
Tian <q135135x@gmail.com>
2021-12-23 21:07:19 +0800

    upload new file

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+// myHeading.textContent ="Hello world!";
+var myImage = document.querySelector('img');
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+myImage.onclick=function(){
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/me.jpg'){
+        myImage.setAttribute('src','images/me2.jpg');        
+    }else{
+        myImage.setAttribute('src', 'images/me.jpg');
+    }
+}
+
+if(!localStorage.getItem('name')){
+    setUserName();
+}else{
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Tian is cool,' + storedName;
+}
+
+myButton.onclick=function(){
+    setUserName();
+}
+
+function setUserName(){
+    let myName = prompt('Please enter your name');
+    if(!myName || myName===null){
+        setUserName();
+    }else{
+        localStorage.setItem('name',myName);
+        myHeading.innerHTML = 'Tian is cool,' + myName;
+    }
+    
+}
